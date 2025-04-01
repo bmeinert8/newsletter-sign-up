2452731dad19efde629cf845f580c962050f39fa
bmeinert8 <brian.meinert1@gmail.com>
2025-04-01 10:36:23 -0400

    implement logic to display success message when email is valid.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //Step 1: Listen for submit button click and capture email input.
 //Step 2: Check is the email is valid using a validation method.
 //Step 3: If the email is invalid, display the error message and style the input with an error state.
-//Step 4: If tje email is valid, hide form display success message.
+//Step 4: If the email is valid, hide form display success message.
 //Step 5: Add functionality to the dismiss button to reset the form.
 
 //Select DOM Elements
@@ -27,6 +27,8 @@ signUpForm.addEventListener('submit', function (event) {
 
   if(isValidEmail) {
     console.log('Email is valid!');
+    newsLetterCard.classList.add('card-hidden');
+    successMessage.classList.remove('success-hidden');
   } else {
     console.log('Email is invalid!');
     errorMessage.classList.remove('error-hidden');
