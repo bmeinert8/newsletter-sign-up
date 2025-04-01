d5f45cbf1af44461c17ac1983594516ac229d471
bmeinert8 <brian.meinert1@gmail.com>
2025-04-01 08:49:19 -0400

    add logic steps to js ad js classes to html and add error lcasses to css

---
+++ main.js
@@ -0,0 +1,6 @@
+//LOGIC STEPS
+//Step 1: Listen for submit button click and capture email input.
+//Step 2: Check is the email is valid using a validation method.
+//Step 3: If the email is invalid, display the error message and style the input with an error state.
+//Step 4: If tje email is valid, hide form display success message.
+//Step 5: Add functionality to the dismiss button to reset the form.
